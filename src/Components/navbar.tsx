0dad74410413fb81ef548735a721539c8652a0fd
Kel <kelstalker@hotmail.com>
2020-04-19 10:40:32 -0600

    getting going

---
+++ src/Components/navbar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface INavBarProps {
+}
+
+export default class  extends React.Component<INavBarProps> {
+  public render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
